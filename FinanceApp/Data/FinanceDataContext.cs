bb642223628da7c9c223421e17211590ca90bd4b
Gabriel Bera <g.bera2710@gmail.com>
2025-09-09 15:59:36 -0300

    first commit

---
+++ FinanceApp/Data/FinanceDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Data
+{
+    public class FinanceDataContext : DbContext
+    {
+        public FinanceDataContext(DbContextOptions<FinanceDataContext> options) : base(options) { }
+    }
+}
