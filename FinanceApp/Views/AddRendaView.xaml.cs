df69b964e27f2edd1277d42ad60ada9e19318dee
Gabriel Bera <g.bera2710@gmail.com>
2026-01-06 02:41:10 -0300

    - Criado as funções da tela addRenda

--- FinanceApp/Views/AddRendaView.xaml.cs
+++ FinanceApp/Views/AddRendaView.xaml.cs
@@ -1,9 +1,90 @@
+using System.Globalization;
+using System.Threading.Tasks;
+using FinanceApp.Data;
+using FinanceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace FinanceApp.Views;
 
 public partial class AddRendaView : ContentPage
 {
-	public AddRendaView()
+    private readonly FinanceDataContext _context;
+    private int _selectedCategoriaId;
+
+	public AddRendaView(FinanceDataContext context)
 	{
 		InitializeComponent();
+        _context = context;
 	}
+
+    private void CVCategoria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var selected = e.CurrentSelection.FirstOrDefault() as Categoria;
+
+        if (selected != null)
+            _selectedCategoriaId = selected.Id;
+        else
+            _selectedCategoriaId = 0;
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            if(valorEntry == null || string.IsNullOrWhiteSpace(valorEntry.Text))
+                await DisplayAlert("Alerta", "Informe um valor", "OK");
+
+            var input = valorEntry.Text.Trim();
+
+            if(!decimal.TryParse(input, NumberStyles.Number, CultureInfo.CurrentCulture, out decimal valor))
+            {
+                await DisplayAlert("Alerta", "Valor inválido", "OK");
+                return;
+            }
+
+            DateTime dataRecebimento = dataRecebimentoPck.Date;
+            var descricao = descricaoEntry.Text;
+
+            var novaRenda = new Receita
+            {
+                Valor = valor,
+                DataDeRecebimento = dataRecebimento,
+                Decricao = descricao,
+                CategoriaId = _selectedCategoriaId
+            };
+
+            _context.Receitas.Add(novaRenda);
+            await _context.SaveChangesAsync();
+
+            await DisplayAlert("Sucesso", "Renda Salva.", "OK");
+            await Shell.Current.GoToAsync("RendaView");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", $"Ocorreu um erro: {ex.Message}", "OK");
+        }
+    }
+
+    public async Task LoadImagens()
+    {
+        try
+        {
+            var categorias = await _context.Categorias
+                                   .Where(c => c.Tipo == CategoriaTipo.Receita)
+                                   .AsNoTracking()
+                                   .ToListAsync();
+
+            CVCategoria.ItemsSource = categorias;
+        }
+        catch(Exception ex)
+        {
+            await DisplayAlert("Ops", $"Falha ao carregar categorias: {ex.Message}", "OK");
+        }
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await LoadImagens();
+    }
 }
--- FinanceApp/obj/Debug/net9.0-ios/ios-arm64/FinanceApp.AssemblyInfo.cs
+++ FinanceApp/obj/Debug/net9.0-ios/ios-arm64/FinanceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c6529caa821825ad307421da0dd43cb446b1e267")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+57cc2739c38dbead496a5af438c3b67d3d05f204")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
